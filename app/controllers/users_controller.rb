500e3ccf61b075d3103095bf06af7f9cd44871bf
Ryan Benedetti <benedett@up.edu>
2015-03-02 17:51:11 -0800

    Lesson 3 partial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+def index
+	@users = User.all
+end
+
+
+#def show
+#end
+
+#def edit	
+#end
+
+#def update
+#end
+
+#def new
+#end
+
+#def create
+#end
+
+#def delete
+#end
+
 end
