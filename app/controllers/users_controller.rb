3fe919e9bf238f0ec8b6d07eb359f2600de072f2
Ryan Benedetti <benedett@up.edu>
2015-03-03 20:43:56 -0800

    Lesson 3 partial commit #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,36 @@ def show
   @user = User.find(params[:id])
 end
 
+def new
+  @user = User.new
+end
+
+def create
+  	@user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "Thanks for signing up!"
+    else
+      render :new
+    end
+end
+
 #def edit	
 #end
 
 #def update
 #end
 
-#def new
-#end
-
-#def create
-#end
 
 #def delete
 #end
+
+private
+
+def user_params
+	params.require(:user).
+	permit(:name, :email, :password, :password_confirmation)
+	
+end
+
 
 end
