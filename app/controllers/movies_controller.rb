4a8e45d0d0a3894a42fea2360fa7f2de24f5f406
Ryan Benedetti <benedett@up.edu>
2016-01-11 11:02:11 -0800

    Adding checkboxes to the form and hadnling form data.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -48,7 +48,7 @@ before_action :require_admin, except: [:index, :show]
 private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name, genre_ids: [])
   end
 end
 
